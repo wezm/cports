ef83b6bef1b43b3f513d722144f6e78cf06d9d39
Wesley Moore <wes@wezm.net>
2024-12-02 12:42:20 +1000

    linkedlist: Only build server binary

--- user/linkedlist/template.py
+++ user/linkedlist/template.py
@@ -1,11 +1,13 @@
 pkgname = "linkedlist"
 # pkgver = "2.0.0_git20241108"
 # until this is fixed: https://github.com/pyinfra-dev/pyinfra/pull/1233
-pkgver = "2.0.11"
-pkgrel = 0
-_gitrev = "b811819"
+pkgver = "2.0.12"
+pkgrel = 1
+_gitrev = "5e1aed8"
 _token = self.get_data("forge_token")
 build_style = "cargo"
+make_build_args = ["--bin", "linkedlistd"]
+make_install_args = [*make_build_args]
 hostmakedepends = ["cargo-auditable", "pkgconf"]
 makedepends = [
     "oniguruma-devel",
@@ -16,11 +18,9 @@ maintainer = "Wesley Moore <wes@wezm.net>"
 license = "custom:none"
 url = "https://forge.wezm.net/wezm/linkedlist"
 source = f"https://forge.wezm.net/api/v1/repos/wezm/linkedlist/archive/{_gitrev}.tar.gz?access_token={_token}>linkedlist-{pkgver}.tar.gz"
-sha256 = "16e1d24c338c4e9c2ae18d6f80d7ae879f997a153ef2fdb7c54c756bbe0c72d9"
+sha256 = "907978d15960b46f96fb1e5afaf3ff8dff888a00711dbe9c887e106314d85d70"
 
 def post_install(self):
     self.install_sysusers(self.files_path / "sysusers.conf")
     self.install_tmpfiles(self.files_path / "tmpfiles.conf")
     self.install_service(self.files_path / "linkedlist")
-    # not needed on server
-    self.uninstall("usr/bin/linkedlist")
