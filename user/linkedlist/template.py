01e4204295b706be51ce328aa3d365a7e23ef27a
Wesley Moore <wes@wezm.net>
2025-01-04 10:34:02 +1000

    linkedlist: 2.0.13

--- user/linkedlist/template.py
+++ user/linkedlist/template.py
@@ -1,9 +1,9 @@
 pkgname = "linkedlist"
 # pkgver = "2.0.0_git20241108"
 # until this is fixed: https://github.com/pyinfra-dev/pyinfra/pull/1233
-pkgver = "2.0.12"
-pkgrel = 1
-_gitrev = "5e1aed8"
+pkgver = "2.0.13"
+pkgrel = 0
+_gitrev = "164baab"
 _token = self.get_data("forge_token")
 build_style = "cargo"
 make_build_args = ["--bin", "linkedlistd"]
@@ -18,7 +18,7 @@ maintainer = "Wesley Moore <wes@wezm.net>"
 license = "custom:none"
 url = "https://forge.wezm.net/wezm/linkedlist"
 source = f"https://forge.wezm.net/api/v1/repos/wezm/linkedlist/archive/{_gitrev}.tar.gz?access_token={_token}>linkedlist-{pkgver}.tar.gz"
-sha256 = "907978d15960b46f96fb1e5afaf3ff8dff888a00711dbe9c887e106314d85d70"
+sha256 = "cb0d4929c7247ee53ceb68dfa390d2fc2456d723d6608b18d26a478504aed9d8"
 
 def post_install(self):
     self.install_sysusers(self.files_path / "sysusers.conf")
