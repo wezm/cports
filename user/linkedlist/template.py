8bfa14240e0483b3780a7d44ffec9200f6c381b6
Wesley Moore <wes@wezm.net>
2025-04-19 11:25:10 +1000

    linkedlist: 2.0.20

--- user/linkedlist/template.py
+++ user/linkedlist/template.py
@@ -1,9 +1,9 @@
 pkgname = "linkedlist"
 # pkgver = "2.0.0_git20241108"
 # until this is fixed: https://github.com/pyinfra-dev/pyinfra/pull/1233
-pkgver = "2.0.19"
+pkgver = "2.0.20"
 pkgrel = 0
-_gitrev = "8792e87"
+_gitrev = "1b83e1e"
 _token = self.get_data("forge_token")
 build_style = "cargo"
 make_build_args = ["--bin", "linkedlistd"]
@@ -17,7 +17,7 @@ pkgdesc = "Linked List web application"
 license = "custom:none"
 url = "https://forge.wezm.net/wezm/linkedlist"
 source = f"https://forge.wezm.net/api/v1/repos/wezm/linkedlist/archive/{_gitrev}.tar.gz?access_token={_token}>linkedlist-{pkgver}.tar.gz"
-sha256 = "2f6beb24ba3a3c17df0f8c22580787ae91ef2291457be47a43dfd5bc1ad81a9d"
+sha256 = "bb12490fc53f9cca253c343b2c8d120d734ffd4e074a5433c1488c924133c216"
 
 def post_install(self):
     self.install_sysusers(self.files_path / "sysusers.conf")
