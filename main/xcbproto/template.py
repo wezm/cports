d2434dddedcefd2342ed52d9726b7da566ad714d
Wesley Moore <wes@wezm.net>
2025-04-26 16:53:17 +1000

    main/xcbproto: switch to different source

--- main/xcbproto/template.py
+++ main/xcbproto/template.py
@@ -8,8 +8,10 @@ depends = ["python"]
 pkgdesc = "XML-XCB (X C Bindings) protocol descriptions"
 license = "MIT"
 url = "https://xcb.freedesktop.org"
-source = f"https://gitlab.freedesktop.org/xorg/proto/xcbproto/-/archive/xcb-proto-{pkgver}/xcbproto-xcb-proto-{pkgver}.tar.gz"
-sha256 = "479447448281cfb6585ad780f23bd75311af20daf344fb9209c8a87ea77e296a"
+#source = f"https://gitlab.freedesktop.org/xorg/proto/xcbproto/-/archive/xcb-proto-{pkgver}/xcbproto-xcb-proto-{pkgver}.tar.gz"
+source = f"https://xorg.freedesktop.org/archive/individual/proto/xcb-proto-{pkgver}.tar.gz"
+
+sha256 = "392d3c9690f8c8202a68fdb89c16fd55159ab8d65000a6da213f4a1576e97a16"
 
 
 def post_install(self):
