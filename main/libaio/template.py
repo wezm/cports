a8765e9c12e0a784fda4a3045fde19b714ed12db
Wesley Moore <wes@wezm.net>
2025-04-26 16:51:19 +1000

    main/libaio: skip tests on x86 for now

--- main/libaio/template.py
+++ main/libaio/template.py
@@ -12,6 +12,16 @@ url = "https://pagure.io/libaio"
 source = f"{url}/archive/libaio-{pkgver}/libaio-libaio-{pkgver}.tar.gz"
 sha256 = "716c7059703247344eb066b54ecbc3ca2134f0103307192e6c2b7dab5f9528ab"
 hardening = ["!ssp"]
+# maybe filled in later
+options = []
+
+if self.profile().arch == "x86":
+    # fails:
+    # Starting cases/20.p
+    # aio_max_nr: 65536
+    # Creating 65536 ioctx-s with 1 events each...
+    # Killed
+    options += ["!check"]
 
 
 @subpackage("libaio-devel")
