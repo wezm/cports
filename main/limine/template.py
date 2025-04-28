d92504c4bfcd864636294c9c729488f850a7a511
Wesley Moore <wes@wezm.net>
2025-04-28 13:04:46 +1000

    main/limine: support x86
    Limine doesn't officially support i586. They say:
    
    > For 32-bit x86 systems, support is only ensured starting with those with
    > Pentium Pro (i686) class CPUs.
    
    We'll see how it goes.

--- main/limine/template.py
+++ main/limine/template.py
@@ -2,7 +2,7 @@ pkgname = "limine"
 pkgver = "9.2.3"
 pkgrel = 0
 # these targets implemented
-archs = ["aarch64", "loongarch64", "riscv64", "x86_64"]
+archs = ["aarch64", "loongarch64", "riscv64", "x86_64", "x86"]
 build_style = "gnu_configure"
 configure_args = ["--enable-all", "TOOLCHAIN_FOR_TARGET=llvm"]
 hostmakedepends = ["automake", "mtools", "nasm"]
