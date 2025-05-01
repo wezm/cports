04a86afcbe80f146679e49e42c0fe85a934083b8
Wesley Moore <wes@wezm.net>
2025-05-01 22:32:54 +1000

    Add linux-alix-lts

---
+++ main/linux-alix-lts/template.py
@@ -0,0 +1,56 @@
+pkgname = "linux-alix-lts"
+pkgver = "6.12.24"
+pkgrel = 0
+archs = [
+    "x86",
+]
+build_style = "linux-kernel"
+configure_args = ["FLAVOR=generic", f"RELEASE={pkgrel}"]
+make_dir = "build"
+make_install_env = {"ZSTD_CLEVEL": "9"}
+hostmakedepends = ["base-kernel-devel"]
+depends = ["base-kernel"]
+provides = ["linux"]
+pkgdesc = f"Linux kernel {pkgver[0 : pkgver.rfind('.')]}.x"
+license = "GPL-2.0-only"
+url = "https://kernel.org"
+source = f"https://cdn.kernel.org/pub/linux/kernel/v{pkgver[0]}.x/linux-{pkgver}.tar.xz"
+sha256 = "643142c1b5991560dd12f950825cc19e4497b95b82641918ecff1177f4130c1d"
+# no meaningful checking to be done
+options = [
+    "!check",
+    "!debug",
+    "!strip",
+    "!scanrundeps",
+    "!scanshlibs",
+    "!lto",
+    "textrels",
+    "execstack",
+    "foreignelf",  # vdso32
+]
+
+if self.current_target == "custom:generate-configs":
+    hostmakedepends += ["base-cross", "ncurses-devel"]
+
+if self.profile().cross:
+    broken = "linux-devel does not come out right"
+
+
+@subpackage("linux-alix-lts-devel")
+def _(self):
+    self.depends += ["clang"]
+    self.options = ["foreignelf", "execstack", "!scanshlibs"]
+    return ["usr/src", "usr/lib/modules/*/build"]
+
+
+@subpackage("linux-alix-lts-dbg", self.build_dbg)
+def _(self):
+    self.options = [
+        "!scanrundeps",
+        "!strip",
+        "!scanshlibs",
+        "foreignelf",
+        "execstack",
+        "textrels",
+    ]
+    return ["usr/lib/debug", "usr/lib/modules/*/apk-dist/boot/System.map-*"]
