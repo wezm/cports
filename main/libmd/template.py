1536aee6fc7a22c15e8086f0c191c97fd65bd5d5
Wesley Moore <wes@wezm.net>
2025-08-30 15:04:23 +1000

    main/libmd: move from user
    When bootstrapping on my Arch Linux system, the libmd I have installed
    keys picked up by the libarchive configure script with no way to turn
    it off, so just make it available.

--- main/libmd/template.py
+++ main/libmd/template.py
@@ -10,7 +10,9 @@ license = "BSD-3-Clause AND BSD-2-Clause AND ISC AND Beerware AND custom:none"
 url = "https://www.hadrons.org/software/libmd"
 source = f"https://archive.hadrons.org/software/libmd/libmd-{pkgver}.tar.xz"
 sha256 = "1bd6aa42275313af3141c7cf2e5b964e8b1fd488025caf2f971f43b00776b332"
-
+options = ["bootstrap"]
+if self.stage == 0:
+    options += ["!scanrundeps"]
 
 def post_install(self):
     self.install_license("COPYING")
