bafcd6515d95200bed7d34979cb3b2203a25028b
Wesley Moore <wes@wezm.net>
2025-08-30 10:34:16 +1000

    main/xz: do not scan runtime dependencies in stage 0

--- main/xz/template.py
+++ main/xz/template.py
@@ -11,7 +11,8 @@ url = "https://tukaani.org/xz"
 source = f"https://github.com/tukaani-project/xz/releases/download/v{pkgver}/xz-{pkgver}.tar.gz"
 sha256 = "507825b599356c10dca1cd720c9d0d0c9d5400b9de300af00e4d1ea150795543"
 options = ["bootstrap"]
-
+if self.stage == 0:
+    options += ["!scanrundeps"]
 
 if self.stage > 0:
     makedepends += ["linux-headers"]
