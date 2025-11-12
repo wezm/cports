99fd3a360338aaa128e04913a29658267bf7a9c5
Wesley Moore <wes@wezm.net>
2025-11-12 19:54:44 +1000

    user/esbuild: new package

---
+++ user/esbuild/template.py
@@ -0,0 +1,15 @@
+pkgname = "esbuild"
+pkgver = "0.28.0"
+pkgrel = 0
+build_style = "go"
+make_build_args = ["./cmd/esbuild"]
+hostmakedepends = ["go"]
+pkgdesc = "JavaScript and CSS bundler and minifier"
+license = "MIT"
+url = "https://esbuild.github.io"
+source = f"https://github.com/evanw/esbuild/archive/v{pkgver}.tar.gz"
+sha256 = "7aae83b197db3fd695e6f378d30fd6cbddeb93e4b1057b2c41d36ecb1dfebbc2"
+
+
+def post_install(self):
+    self.install_license("LICENSE.md")
