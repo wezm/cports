7c5fa5d81a2dd022795b512d1080d95ac66e4d88
q66 <q66@chimera-linux.org>
2026-03-08 17:57:23 +0100

    cbuild: make black+flake8 or ruff a prerequisite for build
    Now templates will refuse to build if incorrectly formatted or
    similar. Linter and formatter is required to be present unless
    overridden to skip it in config.ini.

--- .ci/setup-cbuild.sh
+++ .ci/setup-cbuild.sh
@@ -13,6 +13,9 @@ jobs = 8
 ccache = yes
 # they will not be packaged, but we can still CI them (no public artifacts)
 allow_restricted = yes
+# we lint in separate step
+linter = none
+formatter = none
 EOF
 
 echo "=> Generating cbuild key..."
