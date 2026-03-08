66073bde882981e74696d2439b97dd21d3950891
Wesley Moore <wes@wezm.net>
2026-03-08 11:16:34 +1000

    main/iperf: update to 3.20

--- main/iperf/template.py
+++ main/iperf/template.py
@@ -1,5 +1,5 @@
 pkgname = "iperf"
-pkgver = "3.19.1"
+pkgver = "3.20"
 pkgrel = 0
 build_style = "gnu_configure"
 make_dir = "."
@@ -13,7 +13,7 @@ license = "BSD-3-Clause-LBNL"
 url = "https://github.com/esnet/iperf"
 # source = f"{url}/releases/download/{pkgver}/iperf-{pkgver}.tar.gz"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "85e480d7fffdcb1368888aaee9d76bcfc211e17c2a6dcb2060b281498f82c97b"
+sha256 = "84640ea0f43831850434e50134d0554b7a94f97fb02e2488ffbe252c9fb05a56"
 
 
 def post_install(self):
