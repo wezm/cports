61ce5fde0721bf38d1dfd9c98b4709fd5a0a10ab
Wesley Moore <wes@wezm.net>
2026-03-13 06:55:22 +1000

    main/openjdk21-bootstrap: bootstrap from upstream archive

--- main/openjdk21-bootstrap/template.py
+++ main/openjdk21-bootstrap/template.py
@@ -16,29 +16,30 @@ makedepends = [
 pkgdesc = "Bootstrap binaries of OpenJDK 21"
 license = "GPL-2.0-only WITH Classpath-exception-2.0"
 url = "https://openjdk.org"
-source = f"https://repo.chimera-linux.org/distfiles/openjdk-bootstrap-{pkgver}-{self.profile().arch}.tar.xz"
+# source = f"https://repo.chimera-linux.org/distfiles/openjdk-bootstrap-{pkgver}-{self.profile().arch}.tar.xz"
+source = "https://download.java.net/java/GA/jdk21.0.2/f2283984656d49d69e91c558476027ac/13/GPL/openjdk-21.0.2_linux-x64_bin.tar.gz"
 options = ["!strip"]
 
 match self.profile().arch:
-    case "aarch64":
-        sha256 = (
-            "bc10a4bf011872647d67f391b8cb9d652ddebd7be67b63f4f6e8a278c31f4ecd"
-        )
-    case "ppc64":
-        sha256 = (
-            "99e7de2f490b7686aa76fa08ac2295252bfd39df23202cff9c3378539fca56b4"
-        )
-    case "ppc64le":
-        sha256 = (
-            "f3a28eef1bff5a019af63223fdd4319cfe5f8f37008cc868ff6207c1f48efec8"
-        )
-    case "riscv64":
-        sha256 = (
-            "faf696996a5a5be389a319b64ff5266362ba870b010b40d9e47896399462537f"
-        )
+    # case "aarch64":
+    #     sha256 = (
+    #         "bc10a4bf011872647d67f391b8cb9d652ddebd7be67b63f4f6e8a278c31f4ecd"
+    #     )
+    # case "ppc64":
+    #     sha256 = (
+    #         "99e7de2f490b7686aa76fa08ac2295252bfd39df23202cff9c3378539fca56b4"
+    #     )
+    # case "ppc64le":
+    #     sha256 = (
+    #         "f3a28eef1bff5a019af63223fdd4319cfe5f8f37008cc868ff6207c1f48efec8"
+    #     )
+    # case "riscv64":
+    #     sha256 = (
+    #         "faf696996a5a5be389a319b64ff5266362ba870b010b40d9e47896399462537f"
+    #     )
     case "x86_64":
         sha256 = (
-            "d0fa05f629fae165ff021a143c08493b7b07ca785c237d0852197f78d28a4c3d"
+            "a2def047a73941e01a73739f92755f86b895811afb1f91243db214cff5bdac3f"
         )
     case _:
         broken = f"not yet built for {self.profile().arch}"
