d2dd57b187368ac749433a76dab89a1682437b29
Wesley Moore <wes@wezm.net>
2026-03-13 06:46:51 +1000

    main/gnu-getopt: remove

--- main/psmisc/template.py
+++ main/psmisc/template.py
@@ -3,16 +3,12 @@ pkgver = "23.7"
 pkgrel = 1
 build_style = "gnu_configure"
 hostmakedepends = ["gettext-devel", "automake"]
-makedepends = ["ncurses-devel", "gnu-getopt", "linux-headers"]
+makedepends = ["ncurses-devel", "linux-headers"]
 pkgdesc = "Small utilities that use the proc file-system"
 license = "GPL-2.0-or-later"
 url = "https://gitlab.com/psmisc/psmisc"
 source = f"$(SOURCEFORGE_SITE)/psmisc/psmisc-{pkgver}.tar.xz"
 sha256 = "58c55d9c1402474065adae669511c191de374b0871eec781239ab400b907c327"
-tool_flags = {
-    "LDFLAGS": ["-lgnu_getopt"],
-    "CFLAGS": ["-Dgetopt_long_only=gnu_getopt_long_only"],
-}
 hardening = ["vis", "cfi"]
 # dejagnu
 options = ["!check"]
