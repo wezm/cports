595862567586e1f42c1f5211454d491658ff8671
Wesley Moore <wes@wezm.net>
2026-03-16 11:20:59 +1000

    main/firefox: allow widevine

--- main/firefox/files/vendor.js
+++ main/firefox/files/vendor.js
@@ -11,10 +11,6 @@ pref("extensions.shownSelectionUI", true);
 // Disable some advertising tile garbage on the new tab page
 pref("browser.topsites.contile.enabled", false);
 
-// Does not work on musl (proprietary)
-pref("media.gmp-widevinecdm.visible", false);
-pref("media.gmp-widevinecdm.enabled", false);
-
 // Hangs sending pings on stop sometimes and prevents FF close
 pref("toolkit.telemetry.shutdownPingSender.enabled", false);
 
