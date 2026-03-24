221b33008b97856377dd3de1d99deef50681c55c
Wesley Moore <wes@wezm.net>
2026-03-25 08:50:05 +1000

    user/forgejo-runner: update to 12.7.3

--- user/forgejo-runner/template.py
+++ user/forgejo-runner/template.py
@@ -1,5 +1,5 @@
 pkgname = "forgejo-runner"
-pkgver = "12.7.2"
+pkgver = "12.7.3"
 pkgrel = 0
 build_style = "go"
 hostmakedepends = ["go"]
@@ -7,7 +7,7 @@ pkgdesc = "Task runner for Forgejo"
 license = "GPL-3.0-only"
 url = "https://code.forgejo.org/forgejo/runner"
 source = f"{url}/archive/v{pkgver}.tar.gz"
-sha256 = "8085a66ad64cdd19c35af82d4f59b033dbf3f4b1302b6d2ec785a3059520d799"
+sha256 = "a9a9b37adb6aa44707be90d3003870f5940424b54b0ad39a808a8e60dbfd649a"
 # check: needs network access
 options = ["!check"]
 
