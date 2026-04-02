eb15a86cdba65b20ae670332f46013da061620aa
Wesley Moore <wes@wezm.net>
2026-04-02 18:32:43 +1000

    user/gleam: update to 1.15.2
    Fixes build.

--- user/gleam/template.py
+++ user/gleam/template.py
@@ -1,5 +1,5 @@
 pkgname = "gleam"
-pkgver = "1.14.0"
+pkgver = "1.15.2"
 pkgrel = 0
 build_style = "cargo"
 hostmakedepends = ["cargo-auditable"]
@@ -11,7 +11,7 @@ url = "https://gleam.run"
 source = (
     f"https://github.com/gleam-lang/gleam/archive/refs/tags/v{pkgver}.tar.gz"
 )
-sha256 = "2463831e404762b0a759db874907ab475474535ac2e976a9f249196e34ece054"
+sha256 = "6fe365a52660d854a73d56e0752fc9ff47fdaa19b0ddc9edbcaa1fb935685eca"
 
 
 def install(self):
