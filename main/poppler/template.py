8406b74adb2b258588d5788cdc1f092d338204c7
q66 <q66@chimera-linux.org>
2026-04-23 16:46:08 +0200

    main/poppler: update to 26.04.0

--- main/poppler/template.py
+++ main/poppler/template.py
@@ -1,7 +1,7 @@
 pkgname = "poppler"
-pkgver = "26.02.0"
+pkgver = "26.04.0"
 _test_commit = "9d5011815a14c157ba25bb160187842fb81579a5"
-pkgrel = 1
+pkgrel = 0
 build_style = "cmake"
 configure_args = [
     "-DENABLE_UNSTABLE_API_ABI_HEADERS=ON",
@@ -44,7 +44,7 @@ source = [
 ]
 source_paths = [".", "testdata"]
 sha256 = [
-    "dded8621f7b2f695c91063aab1558691c8418374cd583501e89ed39487e7ab77",
+    "b0955163114af96bc0106f68cb24daf973a629462453d8b82775f81b0d4e0693",
     "c4cbdbf44f1d5c1ccbd7de611e979d97b703851970819cbb021f97218a445ed2",
 ]
 # check_qt6_goostring crashes
