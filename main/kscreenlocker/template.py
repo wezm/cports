2af00ca8a86e62d5e7fe8ec5f9994c5a7c9daa3d
q66 <q66@chimera-linux.org>
2026-04-27 21:24:03 +0200

    main/kscreenlocker: switch tests away from xorg

--- main/kscreenlocker/template.py
+++ main/kscreenlocker/template.py
@@ -7,8 +7,7 @@ configure_args = ["-DCMAKE_INSTALL_LIBEXECDIR=/usr/lib"]
 # circular plasma-workspace dep (QML org.kde.plasma.private.sessions) needed by kscreenlocker_greet,
 # ksmserver-ksldTest even needs it installed under /usr/lib/libexec
 make_check_args = ["-E", "(kscreenlocker-kill|ksmserver-ksld)Test"]
-# ksmserver-x11LockerTest only passes under Xvfb
-make_check_wrapper = ["dbus-run-session", "xvfb-run"]
+make_check_wrapper = ["dbus-run-session", "wlheadless-run", "--"]
 hostmakedepends = [
     "cmake",
     "extra-cmake-modules",
@@ -33,7 +32,7 @@ makedepends = [
     "qt6-qtbase-private-devel",  # qtx11extras_p.h
     "qt6-qtdeclarative-devel",
 ]
-checkdepends = ["dbus", "xserver-xorg-xvfb"]
+checkdepends = ["dbus", "xwayland-run"]
 depends = ["kdeclarative"]
 pkgdesc = "KDE Library and components for secure lock screen architecture"
 license = "GPL-2.0-or-later AND (GPL-2.0-only OR GPL-3.0-only)"
